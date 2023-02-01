18f850903541af43ff2874dc47c56b8e01a96bd8
Hilal38 <hhilalkocak@gmail.com>
2023-02-01 22:43:50 +0300

    added post via python shell to blog

---
+++ blog/new.py
@@ -0,0 +1,16 @@
+#Python shell commands
+from .models import Post
+Post.objects.exclude(id__in=[1,2,]) # There is one =
+Post.objects.exclude(id__in=[1,2,]).delete()
+items= Post.objects.all()
+for id in range(21):
+    if not id%2==0:
+        obj=items[0]
+    else:
+        obj=items[1]
+    obj.pk=None
+    obj.title = f"{obj.title} - {id}" #we changed title info
+    #we forgot slug info
+    obj.save()
+
+    i=Post.objects.last()
---
+++ blog/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+app_name='blog'
+
+urlpatterns = [
+    #path('<slug:blog_slug>/', page_view, name="page_view"), #it must same with page view paramter =page_slug
+]
+
